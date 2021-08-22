7c8f039e7f94c708a63181abd7ec7894c50906f9
Zayon84 <dalmkvist@hotmail.com>
2021-08-22 15:37:03 +0200

    Prefabs Clean up

--- Assets/Scripts/Network/GameSetupManager.cs
+++ Assets/Scripts/Network/GameSetupManager.cs
@@ -14,6 +14,14 @@ public class GameSetupManager : MonoBehaviour
     private void CreatePlayer()
     {
         Debug.Log("GameSetupManager: Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(-5,-7,0), Quaternion.identity);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(5, -7, 0), Quaternion.identity);
+        }
     }
 }
